e243061178f37b0f8fd627b42db3dc5d53088cb5
Dalewhlrr <107579063+Dalewhlrr@users.noreply.github.com>
2024-06-04 12:38:35 -0400

    Create install_mqtt.py

---
+++ install_mqtt.py
@@ -0,0 +1,33 @@
+import subprocess
+import time
+
+def install_docker():
+    print("Installing Docker...")
+    time.sleep(2)
+    subprocess.run(['sudo', 'apt-get', 'update'], check=True)
+    subprocess.run(['sudo', 'apt-get', 'install', '-y', 'docker.io'], check=True)
+    print("Installing Docker-Compose...")
+    time.sleep(2)
+    subprocess.run(['sudo', 'apt-get', 'install', '-y', 'docker.io'], check=True)
+    print("Done!")
+    time.sleep(2)
+
+
+def configure_mosquitto():
+    print("Creating Directories")
+    time.sleep(2)
+    subprocess.run(['mkdir', '-p', '/mosquitto/config'], check=True)
+    subprocess.run(['mkdir', '-p', '/mosquitto/data'], check=True)
+    subprocess.run(['mkdir', '-p', '/mosquitto/log'], check=True)
+    print("Done!")
+    time.sleep(2)
+    print("Starting Container...")
+    time.sleep(2)
+    subprocess.run(["docker", "run", "-d", "-p", "1883:1883", "-p", "9001:9001", "-v", "/mosquitto/data", "-v", "/mosquitto/log", "eclipse-mosquitto"], check=True)
+    subprocess.run(["docker", "ps"])
+
+
+if __name__ == "__main__":
+    install_docker()
+    configure_mosquitto()
+
